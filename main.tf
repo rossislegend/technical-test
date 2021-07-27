51e1e507001e6c61df45c3986bdedc3f1c36a565
rossislegend <rossphillip19@yahoo.co.uk>
2021-07-27 18:38:51 +0100

    Added second instance

--- main.tf
+++ main.tf
@@ -33,14 +33,50 @@ resource "aws_security_group" "ec2a38c443" {
     }
 }
 
-resource "aws_instance" "ec2-instance" {
+resource "aws_instance" "ec2-instance1" {
     ami = "ami-0194c3e07668a7e36"
-    key_name = "EC2_Instance"
+    key_name = "EC2_Instance1"
     vpc_security_group_ids = [aws_security_group.ec2a38c443.id]
     instance_type = "t2.micro"
 
     tags = {
-        Name = "EC2_instance"
+        Name = "EC2_Instance1"
+    }
+
+    ebs_optimized = false
+    iam_instance_profile = "EC2-read-s3"
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = 8
+    }
+
+    user_data = <<EOF
+#!/bin/bash
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y awscli
+sudo apt install -y nginx
+
+aws s3 cp s3://html-s3bucket/index.html index.html
+sudo mv ./index.html /var/www/html/index.html
+
+sudo systemctl restart nginx
+EOF
+
+  lifecycle {
+      create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "ec2-instance2" {
+    ami = "ami-0194c3e07668a7e36"
+    key_name = "EC2_Instance2"
+    vpc_security_group_ids = [aws_security_group.ec2a38c443.id]
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "EC2_Instance2"
     }
 
     ebs_optimized = false
